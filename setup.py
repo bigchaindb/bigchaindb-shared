8ca89edcd4551ae713ed4523c7e0ccfbad8d61a6
Scott Sadler <scott@alphanode.co>
2017-07-16 19:56:48 +0200

    0.1.0

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-
-import os.path
-cabalfile = os.path.join(os.path.dirname(__file__),
-        'ext/bigchaindb-hs/bigchaindb.cabal')
-ver = [l.split()[1] for l in open(cabalfile) if l.startswith('version:')][0]
+from setuptools import setup
 
 
-if __name__ == '__main__':
-    setup(name='bigchaindb-shared',
-          author='BigchainDB Developers',
-          author_email='dev@bigchaindb.com',
-          version=ver,
-          description=
-            ('Prebuilt shared object contaning functions to read and '
-             'create BigchainDB transactions'),
-          packages=['libs', 'bigchaindb_shared'],
-          py_modules=['bigchaindb_shared'],
-          package_data={'libs': ['*.so', 'libs/*.so']},
-          url='https://github.com/libscott/bigchaindb-shared/',
-    )
+setup(name='bigchaindb-shared',
+      author='BigchainDB Developers',
+      author_email='dev@bigchaindb.com',
+      version='0.1.0',
+      description=
+        ('Prebuilt shared object contaning functions to read and '
+         'create BigchainDB transactions'),
+      packages=['lib', 'bigchaindb_shared'],
+      py_modules=['bigchaindb_shared'],
+      package_data={'lib': ['*.so', 'libs/*.so']},
+      url='https://github.com/libscott/bigchaindb-shared/',
+)
