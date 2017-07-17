f0cf2bb3f27a14c3b71b190b372d863b73e53a33
Scott Sadler <scott@alphanode.co>
2017-07-17 13:31:33 +0200

    add apache2 license

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import setup
 
 
 setup(name='bigchaindb-shared',
-      author='BigchainDB Developers',
+      author='BigchainDB',
       author_email='dev@bigchaindb.com',
       version='0.1.0a1',
       description=
         ('Prebuilt shared object contaning functions to read and '
          'create BigchainDB transactions'),
-      packages=['bigchaindb_shared'],
-      py_modules=['bigchaindb_shared'],
-      package_data={'bigchaindb_shared': ['lib/*/*.so', 'lib/*/libs/*.so']},
       url='https://github.com/bigchaindb/bigchaindb-shared/',
+      license="Apache Software License 2.0",
+      packages=['bigchaindb_shared'],
+      package_data={'bigchaindb_shared': ['lib/*/*.so', 'lib/*/libs/*.so']},
+      test_suite='tests',
 )
